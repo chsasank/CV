de71fc6730c21e383060c52da341fd71e960b808
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2015-04-26 07:24:29 +0530

    added new images

--- GrabCut/graph_cuts.m
+++ GrabCut/graph_cuts.m
@@ -1,6 +1,6 @@
 %% Read image and take input; initialize
-clc;clear; close all; addpath('GCMex'); addpath('gco-v3.0/matlab')
-im_clr = imread('rose_small.jpg');
+clc;clear; close all; addpath('gco-v3.0/matlab')
+im_clr = imread('cat_small.jpg');
 im = rgb2gray(im_clr);
 rect = rectInput(im_clr); % Take input
 sz = size(im);
