679aaa7d0bcee40a04a97ba55215249104490154
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2015-04-30 08:26:15 +0530

    trying to add matte

--- GrabCut/test.m
+++
@@ -1,8 +0,0 @@
-mu1 = [1 2];
-Sigma1 = [2 0; 0 0.5];
-mu2 = [-3 -5];
-Sigma2 = [1 0;0 1];
-rng(1); % For reproducibility
-X = [mvnrnd(mu1,Sigma1,1000);mvnrnd(mu2,Sigma2,1000)];
-
-GMModel = fitgmdist(X,2);
